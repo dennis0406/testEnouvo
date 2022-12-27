a45cb3d3b0b88bf23d82435275968a2333a4699b
dennis <laplepnv23@gmail.com>
2022-12-27 17:34:00 +0700

    Initialize and setup project

---
+++ src/utils/Color.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  white: '#fff',
+  black: '#000',
+  Primary: '#ED8B00',
+  bgOverlay: '#00000099',
+  secondary: '#171C8F',
+  grey: '#e4e3e8',
+  red: '#D30707',
+  bgCloseBtn: '#444444',
+};
